f5fc7363d91e3eec5c58e247f78454b11b6d4518
layla <lkesssler3@cnm.edu>
2021-09-28 15:20:30 -0600

    Implemented F-to-C conversion and read from command line

--- src/test/java/edu/cnm/deepdive/TemperatureConverterTest.java
+++ src/test/java/edu/cnm/deepdive/TemperatureConverterTest.java
@@ -5,9 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TemperatureConverterTest {
-
-  static final double[] celsiusTemperatures = {-40,0,100};
-  static final double[] fahrenheitTemperatures = {-40, 32, 212};
+  static final double FAHRENHEIT_TOLERANCE = 0.001;
+  static final double CELSUIS_TOLERANCE = 0.0005;
+  static final double[] celsiusTemperatures = {-40, 0, 100};
+  static final double[] fahrenheitTemperatures = {-40, 32, 212, 33.};
 
   @Test
   void convertC2F() {
@@ -16,10 +17,9 @@ class TemperatureConverterTest {
       double celsius = celsiusTemperatures[i];
       double expectedFahrenheit = fahrenheitTemperatures[i];
       double actualFahrenheit = TemperatureConverter.convertC2F(celsius);
-      assertEquals(expectedFahrenheit, actualFahrenheit);
+      assertEquals(expectedFahrenheit, actualFahrenheit,FAHRENHEIT_TOLERANCE);
+
     }
-
-
 
 
   }
